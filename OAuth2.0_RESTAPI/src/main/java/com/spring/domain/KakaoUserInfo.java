bc783256c0c3073505f3c194b128aa48bb0d968d
SeongHyeok_Lee <dltjdgur327@gmail.com>
2019-01-18 11:01:55 +0900

    kakao login

---
+++ OAuth2.0_RESTAPI/src/main/java/com/spring/domain/KakaoUserInfo.java
@@ -0,0 +1,23 @@
+package com.spring.domain;
+
+import lombok.Data;
+
+@Data
+public class KakaoUserInfo {
+
+	private long id;
+	private properties properties;
+	private kakao_account kakao_account;
+
+	@Data
+	public class properties {
+		private String nickname;
+		private String profile_image;
+		private String thumbnail_image;
+	}
+
+	@Data
+	public class kakao_account{
+		private String email;
+	}
+}
