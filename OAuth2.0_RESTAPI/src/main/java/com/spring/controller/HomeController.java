1d96df3b73d63b4024880eade0e47d009a1a5a21
SeongHyeok_Lee <dltjdgur327@gmail.com>
2019-01-20 19:40:56 +0900

    RequestMapping > GetMapping

--- OAuth2.0_RESTAPI/src/main/java/com/spring/controller/HomeController.java
+++ OAuth2.0_RESTAPI/src/main/java/com/spring/controller/HomeController.java
@@ -1,24 +1,24 @@
 package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/google")
+	@GetMapping("/google")
 	public String google() {
 
 		return "/google";
 	}
 
-	@RequestMapping("/facebook")
+	@GetMapping("/facebook")
 	public String facebook() {
 
 		return "/facebook";
 	}
 
-	@RequestMapping("/kakao")
+	@GetMapping("/kakao")
 	public String kakao() {
 
 		return "/kakao";
